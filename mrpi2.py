6ea13eeb37f6c99cac35c1946ae36942c6dbee91
chomik <to.chomik@gmail.com>
2011-11-11 11:03:20 +0100

    Memory usage optimization
    Change points in circle counting mechanism for that which is usess less
    memory. Prefer generators over lists.

--- mrpi2.py
+++ mrpi2.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python
 
-from random import uniform as random
+from random import random
 from math import pi
 from multiprocessing import Pool
 from time import time
 
-r = 1
+# Assume radius equals 1
 num_of_points = 10000000
 num_of_workers = 2
-real_number_of_points = num_of_points/num_of_workers*num_of_workers
-
-def get_point():
-    return (random(-r, r), random(-r,r))
-
-def in_circle(point):
-    return r**2 >= point[0]**2 + point[1]**2
+points_per_worker = num_of_points/num_of_workers
+real_number_of_points = points_per_worker*num_of_workers
 
 def perform_an_experiment(experiment_num_of_points):
-    return len(filter(in_circle,
-        (get_point() for i in xrange(experiment_num_of_points))))
+    in_a_circle = lambda point: 1 >= point[0]**2 + point[1]**2
+    return sum(
+            (
+                in_a_circle((random(), random()))
+                for i in xrange(experiment_num_of_points)
+            )
+            )
 
 workers = Pool(processes=num_of_workers)
 
 start = time()
 num_of_points_in_circle = workers.map(perform_an_experiment,
-        (real_number_of_points/num_of_workers for i in xrange(num_of_workers)))
+        (points_per_worker for i in xrange(num_of_workers)))
 num_of_points_in_circle = reduce(lambda x,y: x+y, num_of_points_in_circle)
 
 mypi = float(num_of_points_in_circle) / float(real_number_of_points) * 4.0
--- pi.py
+++ pi.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python
 
-from random import uniform as random
+from random import random
 from math import pi
 from time import time
 
-r = 1
+# Assume that radius equals 1.
 num_of_points = 10000000
 
-def get_point():
-    return (random(-r,r), random(-r,r))
-
-def in_a_circle(point):
-    return r**2 >= point[0]**2 + point[1]**2
+in_a_circle = lambda point: 1 >= point[0]**2 + point[1]**2
 
 start = time()
-mypi = 4.0 * float(len(filter(in_a_circle, (get_point() for i in xrange(num_of_points))))) / float(num_of_points)
+mypi = 4.0\
+       * sum((in_a_circle((random(), random())) for i in xrange(num_of_points)))\
+       / float(num_of_points)
 end = time()
 
 print "calculated value of pi: %f" % mypi
