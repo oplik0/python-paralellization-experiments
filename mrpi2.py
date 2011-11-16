f4e0cd4b2253d96c0db9ab7d9e627fe49bf540b4
chomik <to.chomik@gmail.com>
2011-11-16 23:00:47 +0100

    Fix windows issues
    Fixes issues occurring due to wrong encapsulation of instructions.
    Everything that is meant to be executed in main process only must be
    called inside __name__ == "__main__" block.
    
    Besides mrpi.py is slightly optimized to make competition with pi.py
    and mrpi2.py fair.

--- mrpi2.py
+++ mrpi2.py
@@ -13,23 +13,22 @@ real_number_of_points = points_per_worker*num_of_workers
 
 def perform_an_experiment(experiment_num_of_points):
     in_a_circle = lambda point: 1 >= point[0]**2 + point[1]**2
-    return sum(
-            (
+    return sum((
                 in_a_circle((random(), random()))
                 for i in xrange(experiment_num_of_points)
-            )
-            )
+                ))
 
-workers = Pool(processes=num_of_workers)
+if __name__ == "__main__":
+    workers = Pool(processes=num_of_workers)
 
-start = time()
-num_of_points_in_circle = workers.map(perform_an_experiment,
-        (points_per_worker for i in xrange(num_of_workers)))
-num_of_points_in_circle = reduce(lambda x,y: x+y, num_of_points_in_circle)
+    start = time()
+    num_of_points_in_circle = workers.map(perform_an_experiment,
+            (points_per_worker for i in xrange(num_of_workers)))
+    num_of_points_in_circle = reduce(lambda x,y: x+y, num_of_points_in_circle)
 
-mypi = float(num_of_points_in_circle) / float(real_number_of_points) * 4.0
-end = time()
+    mypi = float(num_of_points_in_circle) / float(real_number_of_points) * 4.0
+    end = time()
 
-print "calculated value of pi: %f" % mypi
-print "done in %f seconds" % (end-start)
-print "pi value from math lib: %f" % pi
+    print "calculated value of pi: %f" % mypi
+    print "done in %f seconds" % (end-start)
+    print "pi value from math lib: %f" % pi
